e42b76d3c4c397eee1893e051d8cc25218628f8a
Asliddin Bozorov <asliddiniut@gmail.com>
2024-05-05 22:39:17 +0500

    production error fixed

--- app/@modal/(.)service/modal.jsx
+++ app/@modal/(.)service/modal.jsx
@@ -10,7 +10,7 @@ export function Modal({ children }) {
   const dialogRef = useRef(null);
 
   useEffect(() => {
-    if (!dialogRef.current?.open) {
+    if (typeof window !== "undefined" && !dialogRef.current?.open) {
       dialogRef.current?.showModal();
     }
   }, []);
@@ -19,22 +19,26 @@ export function Modal({ children }) {
     router.back();
   }
 
-  return createPortal(
-    <div className="min-h-screen min-w-full fixed inset-0 bg-black opacity-70 grid place-items-center z-50">
-      <dialog
-        ref={dialogRef}
-        className="bg-gradient w-[90%] lg:w-[80%] max-w-[900px] border-none rounded-xl fixed z-[55] grid place-items-center"
-        onClose={onDismiss}
-      >
-        {children}
-        <button
-          onClick={onDismiss}
-          className="absolute top-2.5 right-2.5 bg-transparent text-2xl"
+  if (typeof window !== "undefined") {
+    return createPortal(
+      <div className="min-h-screen min-w-full fixed inset-0 bg-black opacity-70 grid place-items-center z-50">
+        <dialog
+          ref={dialogRef}
+          className="bg-gradient w-[90%] lg:w-[80%] max-w-[900px] border-none rounded-xl fixed z-[55] grid place-items-center"
+          onClose={onDismiss}
         >
-          <RxCross2 />
-        </button>
-      </dialog>
-    </div>,
-    document.getElementById("modal-root")
-  );
+          {children}
+          <button
+            onClick={onDismiss}
+            className="absolute top-2.5 right-2.5 bg-transparent text-2xl"
+          >
+            <RxCross2 />
+          </button>
+        </dialog>
+      </div>,
+      document.getElementById("modal-root")
+    );
+  } else {
+    return null;
+  }
 }
